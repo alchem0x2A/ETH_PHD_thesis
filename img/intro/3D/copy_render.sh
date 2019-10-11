54a3b76eb12daad11a9cca4d956843543913000d
T.Tian <lovaulonzezoeta@gmail.com>
2019-10-11 15:36:14 +0200

    Add 3D plots

---
+++ img/intro/3D/copy_render.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+bl_file=$1
+remote_bl_file=~/tmp/$bl_file
+# scp
+rsync $bl_file euler:~/tmp/
+echo "File transfer finished"
+
+# ssh render on remote
+render_string="blender -b -noaudio ~/tmp/${bl_file} -o //${bl_file/.blend}_output.png -t 24 -f 1"  
+echo $render_string
+ssh euler "bsub -n 24 \"$render_string\""
+echo "job submitted"
+
+# rsync euler:~/tmp/output*.png ./
